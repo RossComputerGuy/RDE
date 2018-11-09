e73b8af1bdd3411f6ffe8ac2db5086854fad9194
Spaceboy Ross <spaceboyross@yandex.com>
2018-11-09 10:58:39 -0800

    GTK theme for Windows 9x theme

--- share/themes/win9x/index.js
+++ share/themes/win9x/index.js
@@ -1,7 +1,8 @@
-const fs = require("fs");
+const fs = require("fs-extra");
 const os = require("os");
 const path = require("path");
 
 module.exports.init = () => {
 	fs.writeFileSync(path.join(os.homedir(),".local","share","icons","win98start.png"),fs.readFileSync(path.join(__dirname,"images","startbutton.png"));	fs.writeFileSync(path.join(os.homedir(),".config","fbpanel","win9x"),fs.readFileSync(path.join(__dirname,"config","fbpanel.conf"));
+		fs.copySync(path.join(os.homedir(),".theme","Chicago95"),path.join(__dirname,"Chicago95"));
 };
