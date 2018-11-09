9e3be3a08335ee94a48916709db5c5b94919ab00
Spaceboy Ross <spaceboyross@yandex.com>
2018-11-09 11:14:11 -0800

    fixed windows 9x theme

--- share/themes/win9x/index.js
+++ share/themes/win9x/index.js
@@ -1,9 +1,31 @@
-const fs = require("fs-extra");
+const fs = require("fs");
 const os = require("os");
 const path = require("path");
 
+function copyFile(target,source) {
+	var targetFile = target;
+	if(fs.existsSync(target)) {
+		if(fs.lstatSync(target).isDirectory()) targetFile = path.join(target,path.basename(source));
+	}
+	fs.writeFileSync(targetFile,fs.readFileSync(source));
+}
+
+function copyDir(target,source) {
+	var files = [];
+	var targetFolder = path.join(target,path.basename(source));
+	if(!fs.existsSync(targetFolder)) fs.mkdirSync(targetFolder);
+    if(fs.lstatSync(source).isDirectory()) {
+        files = fs.readdirSync(source);
+        files.forEach(file => {
+            var curSource = path.join(source,file);
+            if(fs.lstatSync(curSource).isDirectory()) copyDir(targetFolder,curSource);
+            else copyFile(targetFolder,curSource);
+        });
+    }
+}
+
 module.exports.init = () => {
-	fs.copySync(path.join(os.homedir(),".local","share","icons","win98start.png"),path.join(__dirname,"images","startbutton.png"));
-	fs.copySync(path.join(os.homedir(),".config","fbpanel","win9x"),path.join(__dirname,"config","fbpanel.conf"));
-	fs.copySync(path.join(os.homedir(),".themes","Chicago95"),path.join(__dirname,"Chicago95"));
+	copyFile(path.join(os.homedir(),".local","share","icons","win98start.png"),path.join(__dirname,"images","startbutton.png"));
+	copyFile(path.join(os.homedir(),".config","fbpanel","win9x"),path.join(__dirname,"config","fbpanel.conf"));
+	copyDir(path.join(os.homedir(),".themes","Chicago95"),path.join(__dirname,"Chicago95"));
 };
