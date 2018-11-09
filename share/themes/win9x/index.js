3d63fac21348b4efd84e4b219136347f9c5e7e61
Spaceboy Ross <spaceboyross@yandex.com>
2018-11-09 11:04:44 -0800

    Fixed syntax in windows 9x theme, again

--- share/themes/win9x/index.js
+++ share/themes/win9x/index.js
@@ -3,6 +3,7 @@ const os = require("os");
 const path = require("path");
 
 module.exports.init = () => {
-	fs.writeFileSync(path.join(os.homedir(),".local","share","icons","win98start.png"),fs.readFileSync(path.join(__dirname,"images","startbutton.png"));	fs.writeFileSync(path.join(os.homedir(),".config","fbpanel","win9x"),fs.readFileSync(path.join(__dirname,"config","fbpanel.conf")));
-		fs.copySync(path.join(os.homedir(),".themes","Chicago95"),path.join(__dirname,"Chicago95"));
+	fs.copySync(path.join(os.homedir(),".local","share","icons","win98start.png"),path.join(__dirname,"images","startbutton.png"));
+	fs.copySync(path.join(os.homedir(),".config","fbpanel","win9x"),path.join(__dirname,"config","fbpanel.conf"));
+	fs.copySync(path.join(os.homedir(),".themes","Chicago95"),path.join(__dirname,"Chicago95"));
 };
