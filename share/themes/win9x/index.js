1d18d3226c7d7345a92f249ce72d121f6ff1dd9c
Spaceboy Ross <spaceboyross@yandex.com>
2018-11-09 10:59:07 -0800

    Fixed themes path

--- share/themes/win9x/index.js
+++ share/themes/win9x/index.js
@@ -4,5 +4,5 @@ const path = require("path");
 
 module.exports.init = () => {
 	fs.writeFileSync(path.join(os.homedir(),".local","share","icons","win98start.png"),fs.readFileSync(path.join(__dirname,"images","startbutton.png"));	fs.writeFileSync(path.join(os.homedir(),".config","fbpanel","win9x"),fs.readFileSync(path.join(__dirname,"config","fbpanel.conf"));
-		fs.copySync(path.join(os.homedir(),".theme","Chicago95"),path.join(__dirname,"Chicago95"));
+		fs.copySync(path.join(os.homedir(),".themes","Chicago95"),path.join(__dirname,"Chicago95"));
 };
